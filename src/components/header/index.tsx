b20de0c4d0a7908d4c0696f2a141cb2bcec07e1b
JuFML <julianafernandezlima@hotmail.com>
2025-06-24 05:36:27 +0200

    feat: add conditional icon rendering in header based on auth status

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router"
 import logoImg from "../../assets/logo.svg"
-import { FiUser, FiLogIn } from "react-icons/fi"
+import { FiUser, FiLogIn, FiLogOut } from "react-icons/fi"
+import { useContext } from "react"
+import { AuthContext } from "../../contexts/auth"
 
 const Header = () => {
-  const signed = false
-  const loadingAuth = false
+  const { signed, loadingAuth, logout } = useContext(AuthContext)
 
   return (
     <header className="w-full flex items-center justify-center h-16 bg-white drop-shadow mb-4">
@@ -15,18 +16,24 @@ const Header = () => {
 
         {!loadingAuth && !signed && (
           <Link to="/login">
-            <div className="border-2 rounded-full p-1 border-gray-500">
+            <div className="border-2 rounded-full p-1 border-gray-500 cursor-pointer">
               <FiLogIn size={24} color="#000" />
             </div>
           </Link>
         )}
 
         {!loadingAuth && signed && (
-          <Link to="/dashboard">
-            <div className="border-2 rounded-full p-1 border-gray-500">
-              <FiUser size={24} color="#000" />
+          <div className="flex gap-5">
+            <Link to="/dashboard">
+              <div className="border-2 rounded-full p-1 border-gray-500 cursor-pointer">
+                <FiUser size={24} color="#000" />
+              </div>
+            </Link>
+
+            <div className="border-2 rounded-full p-1 border-gray-500 cursor-pointer" onClick={logout}>
+              <FiLogOut size={24} color="#000" />
             </div>
-          </Link>
+          </div>
         )}
 
       </div>
