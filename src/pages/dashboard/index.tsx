3ead8068d7d90c7d70e87625986120f63389ea55
JuFML <julianafernandezlima@hotmail.com>
2025-06-29 07:54:56 +0200

    feat: create dashboard page showing cars registered by logged-in user only

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,12 +1,41 @@
+import { useEffect, useContext } from "react"
+import CarCard from "../../components/carCard"
+import { where } from "firebase/firestore"
 import DashboardHeader from "../../components/dashboardHeader"
-
+import { AuthContext } from "../../contexts/auth"
+import { useCars } from "../../hooks/useCars"
 
 
 function Dashboard() {
+  const { user } = useContext(AuthContext)
+  const { cars, loadCars } = useCars([where("ownerUID", "==", user?.uid)])
+
+  useEffect(() => {
+    if (!user?.uid) {
+      return
+    }
+    loadCars()
+  }, [user])
 
   return (
     <>
       <DashboardHeader />
+      <section className="bg-white p-4 rounded-lg w-full max-w-3xl mx-auto flex justify-center items-center gap-2">
+        <input
+          type="text"
+          className="w-full border-2 rounded-lg h-9 px-3 outline-none border-gray-200"
+          placeholder="Digite o nome do carro..."
+        />
+        <button className="bg-red-500 h-9 px-8 rounded-lg text-white font-medium text-lg">Buscar</button>
+      </section>
+
+      <h1 className="font-bold text-center mt-6 text-2xl mb-4">Carros novos e usados em todo  Brasil</h1>
+
+      <main className="w-full grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        {cars.map((car) => (
+          <CarCard key={car.id} showDeleteIcon {...car} />
+        ))}
+      </main>
     </>
   )
 }
