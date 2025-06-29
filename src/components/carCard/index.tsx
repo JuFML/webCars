b9d85c7aec0bcd0873512821d96295591f2c108c
JuFML <julianafernandezlima@hotmail.com>
2025-06-29 06:19:22 +0200

    feat: fetch and render car data dynamically from Firebase in Home page

--- src/components/carCard/index.tsx
+++ src/components/carCard/index.tsx
@@ -1,25 +1,43 @@
+import { Link } from "react-router"
+import type { CarProps } from "../../pages/home"
+import { useState } from "react"
 
 
-const CarCard = () => {
+const CarCard = ({ id, city, images, km, name, price, year }: CarProps) => {
+  const [loadedImages, setLoadedImages] = useState<string[]>([])
+
+  const handleImageLoad = (id: string) => {
+    setLoadedImages((prev) => [...prev, id])
+  }
+
   return (
-    <section className="mx-auto bg-white rounded-lg">
-      <img
-        className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
-        src="https://image.webmotors.com.br/_fotos/anunciousados/gigante/2025/202505/20250511/honda-fit-1.5-lx-16v-flex-4p-automatico-wmimagem21243076631.jpg?s=fill&w=552&h=414&q=60"
-        alt="Carro" />
-      <p className="font-bold mt-1 mb-2 px-2">Honda FIT</p>
+    <Link key={id} to={`car/${id}`}>
+      <section className="mx-auto bg-white rounded-lg">
+        <div
+          className="w-full rounded-lg mb-2 h-72 bg-slate-200"
+          style={{ display: loadedImages.includes(id) ? "none" : "block" }}
+        ></div>
+        <img
+          className="w-full rounded-lg mb-2 max-h-72 hover:scale-105 transition-all"
+          src={images[0]}
+          alt="Carro"
+          onLoad={() => handleImageLoad(id)}
+          style={{ display: loadedImages.includes(id) ? "block" : "none" }} />
+        <p className="font-bold mt-1 mb-2 px-2">{name}</p>
 
-      <div className="flex flex-col px-2">
-        <p className="text-zinc-700 mb-6">Ano 2016/2016 | 23.000 km</p>
-        <strong className="text-black font-medium text-xl">R$ 190.000</strong>
-      </div>
+        <div className="flex flex-col px-2">
+          <p className="text-zinc-700 mb-6">Ano {year} | {km} km</p>
+          <strong className="text-black font-medium text-xl">R$ {price}</strong>
+        </div>
 
-      <div className="w-full h-px bg-slate-200 my-2"></div>
+        <div className="w-full h-px bg-slate-200 my-2"></div>
 
-      <div className="px-2 pb-2">
-        <p className="text-black">Campo Grande - MS</p>
-      </div>
-    </section>
+        <div className="px-2 pb-2">
+          <p className="text-black">{city}</p>
+        </div>
+      </section>
+    </Link>
+
   )
 }
 
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,55 @@
+import { useEffect, useState } from "react"
 import CarCard from "../../components/carCard"
+import { collection, getDocs, orderBy, query } from "firebase/firestore"
+import { db } from "../../services/firebaseConnection"
+
+export interface CarProps {
+  id: string;
+  city: string;
+  created: string;
+  description: string;
+  images: string[];
+  km: string;
+  model: string;
+  name: string;
+  owner: string;
+  ownerUID: string;
+  price: string | number;
+  whatsapp: string;
+  year: string;
+}
 
 function Home() {
+  const [cars, setCars] = useState<CarProps[]>([])
+
+  useEffect(() => {
+    const carsRef = collection(db, "cars")
+    const queryRef = query(carsRef, orderBy("created", "desc"))
+
+    const carsList = [] as CarProps[]
+    getDocs(queryRef)
+      .then((snapshot) => {
+        snapshot.forEach(item => {
+          carsList.push({
+            id: item.id,
+            city: item.data().city,
+            created: item.data().created,
+            description: item.data().description,
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            images: item.data().images.map((img: any) => img.url),
+            km: item.data().km,
+            model: item.data().model,
+            name: item.data().name,
+            owner: item.data().owner,
+            ownerUID: item.data().ownerUID,
+            price: item.data().price,
+            whatsapp: item.data().whatsapp,
+            year: item.data().year
+          })
+        })
+        setCars(carsList)
+      })
+  }, [])
 
   return (
     <>
@@ -16,7 +65,9 @@ function Home() {
       <h1 className="font-bold text-center mt-6 text-2xl mb-4">Carros novos e usados em todo  Brasil</h1>
 
       <main className="w-full grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-        <CarCard />
+        {cars.map((car) => (
+          <CarCard key={car.id} {...car} />
+        ))}
       </main>
     </>
   )
